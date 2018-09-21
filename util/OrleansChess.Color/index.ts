880454e19adc2bb9d770c8f8a712bd99991a3bb8
Joey <brownjn12@gmail.com>
2018-09-20 20:58:13 -0500

    got dark pieces working

--- util/OrleansChess.Color/index.ts
+++ util/OrleansChess.Color/index.ts
@@ -1,32 +1,33 @@
-import { Config, LightColors, DarkColors } from "./config";
+import { Config, LightColors, DarkColors, IColor } from "./config";
 import { writeFile, ensureDirectoryExists } from "./util";
 import { readdir, readFile } from "./util";
 
-async function createLightPieces() {
-    await LightColors.forEach(async color => {
-        console.log(`Creating ${color.friendly} pieces`);
-        var destination = `${Config.destination}/${color.friendly}`;
-        await ensureDirectoryExists(destination);
-        var files = (await readdir(Config.source)).filter((x: string)=>x.startsWith('w'));
-        await files.forEach(async sourceFile => {
-            var piece = `${Config.source}/${sourceFile}`;
-            var content = (await readFile(piece)).toString();
-            var newContent = content.split("#ffffff").join(color.hex);
-            var fileName = sourceFile.substring(1);
-            await writeFile(`${destination}/${fileName}`, newContent);
-        });
-    });
-}
-
-async function createDarkPieces() {
-    await DarkColors.forEach(async color => {
-        console.log(`Creating ${color.friendly} pieces`);
+async function createPiece (pieceFilter: (x: string)=>boolean, hexToReplace: string, color: IColor) {
+    console.log(`Creating ${color.friendly} pieces`);
+    var destination = `${Config.destination}/${color.friendly}`;
+    await ensureDirectoryExists(destination);
+    var files = (await readdir(Config.source)).filter(pieceFilter);
+    await files.forEach(async sourceFile => {
+        var piece = `${Config.source}/${sourceFile}`;
+        var content = (await readFile(piece)).toString();
+        var newContent = content.split(hexToReplace).join(color.hex);
+        var fileName = sourceFile.substring(1);
+        await writeFile(`${destination}/${fileName}`, newContent);
     });
 }
 
 async function main() {
-    await createLightPieces();
-    // await createDarkPieces();
+    await LightColors.forEach(async color => {
+        const pieceFilter = (x: string)=>x.startsWith('w');
+        const hexToReplace = "#ffffff";
+        await createPiece(pieceFilter, hexToReplace, color);
+    });
+
+    await DarkColors.forEach(async color => {
+        const pieceFilter = (x: string)=>x.startsWith('b');
+        const hexToReplace = "#000000";
+        await createPiece(pieceFilter, hexToReplace, color);
+    });
 }
 
 main();
