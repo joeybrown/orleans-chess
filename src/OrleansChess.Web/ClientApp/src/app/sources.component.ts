13e858d90eccd75568dec2c135d1f0f26e1d4c19
Joey <brownjn12@gmail.com>
2018-09-24 17:41:34 -0500

    Added Black & White pieces

--- src/OrleansChess.Web/ClientApp/src/app/sources.component.ts
+++ src/OrleansChess.Web/ClientApp/src/app/sources.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 
 
 @Component({
-  selector: 'app-sources',
-  templateUrl: './sources.component.html',
+    selector: 'app-sources',
+    templateUrl: './sources.component.html',
 })
 export class SourcesComponent {
     sources = [
-        {text: 'Orleans', href: 'https://dotnet.github.io/orleans/'},
-        {text: 'chessboard.js', href: 'http://chessboardjs.com'},
-        {text: 'Chess.NET', href: 'https://github.com/ProgramFOX/Chess.NET'}
+        { text: 'Orleans', href: 'https://dotnet.github.io/orleans/' },
+        { text: 'chessboard.js', href: 'http://chessboardjs.com' },
+        { text: 'Chess.NET', href: 'https://github.com/ProgramFOX/Chess.NET' },
+        { text: 'Chess Pieces', href: 'https://commons.wikimedia.org/wiki/Category:SVG_chess_pieces' }
     ]
 }
